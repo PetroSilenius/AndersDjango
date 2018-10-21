143b8e84ceaed12642d48f427699a635a6796c1a
Blomqma <blomqvist_markus@hotmail.com>
2018-10-21 15:07:54 +0300

    added registration feature to localhost/login

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,7 +1,24 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(index)
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup/signup.html', {'form': form})
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=55f0bce1ae50d640b124d24c67f01d62'
