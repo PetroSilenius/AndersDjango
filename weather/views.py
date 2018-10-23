d1b04d3603bbfd7576cefa13effdd4c17f885167
pvsile <pvsile@utu.fi>
2018-10-23 14:26:06 +0300

    add signup and delete

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,39 @@
 import requests
-from django.shortcuts import render
 from .models import City
 from .forms import CityForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(index)
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup/signup.html', {'form': form})
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=e04f7b263bb17254656fab6340e5e633'
 
-    if request.method == 'POST':
+    if 'add city' in request.POST:
         form = CityForm(request.POST)
         form.save()
-
 
     form = CityForm()
 
     cities = City.objects.all()
+
+    if 'delete city' in request.POST:
+        cities.delete()
 
     weather_data = []
 
@@ -31,5 +50,6 @@ def index(request):
 
         weather_data.append(city_weather)
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {'weather_data': weather_data, 'form': form}
+
     return render(request, 'weather/weather.html', context)
